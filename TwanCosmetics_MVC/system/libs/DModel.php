7a2036c3c2d38704ecebc38918f84ea7f197cf85
Nguyễn Tô Đức Tài <101978974+DucTai0909@users.noreply.github.com>
2022-12-03 23:36:18 +0700

    Add files via upload

---
+++ TwanCosmetics_MVC/system/libs/DModel.php
@@ -0,0 +1,22 @@
+<?php
+    class DModel{
+        protected $conn;
+        protected $servername = "localhost";
+        protected $username = "root";
+        protected $password = "";
+        public function connectDB(){
+            try {
+                $conn = new PDO("mysql:host=$this->servername;dbname=twancosmetics_mvc_edit", $this->username, $this->password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //echo "Connected successfully <br>";
+                return $conn;
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }
+    }
+    
+
+    
+?>
