47a8ae1cd6401ddd265303df734d7783efc8e694
lin9601 <nlien3609@gmail.com>
2022-12-19 16:47:33 +0700

    BlogPage

---
+++ Php/BlogPage.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tất cả bài viết</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Css/BlogPage.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
+
+<body>
+    <header data-spy="affix" style="background-color:white">
+        <img class="logo" src="../Images_Web/Logo/Logo.svg">
+        <div class="heading1">
+            CỬA HÀNG TRỰC TUYẾN 24/7 - GIAO HÀNG NHANH CHÓNG
+        </div>
+        <div class="thanhtimkiem">
+            <input class="timkiemsp" type="text" id="mySearch" onkeyup="myFunction()" placeholder="Tìm kiếm sản phẩm" title="Type in a category" />
+            <button type="button" class="material-symbols-outlined" id="icon-search">
+                search
+            </button>
+        </div>
+        <div class="icon2">
+            <button class="material-symbols-outlined" id="icon">
+                shopping_cart
+            </button>
+            <button class="material-symbols-outlined" id="icon">
+                account_circle
+            </button>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#trangchu">Trang chủ</a></li>
+                <li>
+                    <a href="#sanpham">Sản phẩm </a>
+                </li>
+                <li><a href="#khuyenmai">Khuyến mãi</a></li>
+                <li><a href="#kienthuc" class="active">Kiến thức chăm sóc da</a></li>
+                <li><a href="#lienhe">Liên hệ </a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="frame">
+        <ul class="breadcrumb">
+            <li><a href="#trangchu">Trang chủ</a></li>
+            <li>Kiến thức chăm sóc da</li>
+        </ul>
+    </div>
+    <div class="flex-container">
+        <div>
+            <img src="../Images_Web/Posters/Bài viết/255851207_4488384907923767_5021908811223513261_n_d1d0829cfb.jpg" class="image-blog" />
+            <article>
+                <h6>23.09.2021</h6>
+                <a href="#">
+                    DA DẦU, MỤN SẼ “ĂN CHAY” NHƯ THẾ NÀO?
+                </a>
+                <p>
+                    Vẻ đẹp của làn da được xem như một tấm gương phản chiếu tốt nhất về tình trạng sức khỏe thể chất và tinh thần của chúng ta. Giống như các loại da khác, da dầu cũng sẽ đạt được trạng thái khỏe mạnh ...
+                </p>
+            </article>
+        </div>
+        <div>
+            <img src="../Images_Web/Posters/Bài viết/dewy-skin-insta-jin-4.jpg" class="image-blog" />
+            <article>
+                <h6>02.10.2022</h6>
+                <a href="#">XU HƯỚNG LÀM ĐẸP DEWY SKIN “LÀM MƯA LÀM GIÓ” CỘNG ĐỒNG LÀM ĐẸP
+
+                </a>
+                <p>
+                    Trong thời gian gần đây, xu hướng làm đẹp dewy skin với làn da sáng bóng như phủ sương đang liên tục được “lăng xê” trên các sàn diễn thời trang và được giới làm đẹp nhiệt liệt ủng hộ.
+                </p>
+            </article>
+        </div>
+    </div>
+    <img class="logo2" src="../Images_Web/Logo/Logo.svg">
+  <footer>
+    <div class="nav2">
+      <a href="#info">Về chúng tôi </a>
+      <a href="#sanpham">Sản phẩm </a>
+      <a> </a>
+      <a href="#hotro">Hỗ trợ khách hàng</a>
+      <a href="#lienhe">Liên hệ </a>
+    </div>
+    <p>
+      <hr class="line2" />
+    </p>
+
+    <pre class="footer-text">
+          Địa chỉ: Khu phố 6, Phường Linh Trung, Thành phố Thủ Đức, Thành phố Hồ Chí Minh - Email: cskh@hotro.twancosmestics.vn
+                Chịu Trách Nhiệm Quản Lí Nội Dung: Nguyễn Ngọc Hiền - Điện thoại liên hệ: 0812922218
+                © 2022 Twan Comestics. All rights reserved.
+        </pre>
+  </footer>
+</body>
+
+</html>
